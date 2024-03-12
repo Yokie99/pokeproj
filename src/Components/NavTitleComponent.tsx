04f163bb1fd887ccf5faa7344c6a4b43d957b24e
Yokie99 <kyleym@uci.edu>
2024-03-12 13:58:22 -0700

    changing this to main assignment repo

---
+++ src/Components/NavTitleComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavTitleComponent = () => {
+  return (
+    <div>
+        <p>this is content from the NavTitle Component</p>
+    </div>
+  )
+}
+
+export default NavTitleComponent
